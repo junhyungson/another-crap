041c70b2feeffe3a7e62c3f42d733225532722d1
Jun <cybergeron89@gmail.com>
2020-12-03 23:17:53 -0800

    footer and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+
+import Header from './components/Header';
+import Footer from './components/footer';
 
 
 const App = () => {
   return (
-    <div>alallala</div>
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>lalalalalal</h1>
+        </Container>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
